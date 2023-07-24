b44378f10551d8a0478b4f02ab752fdde0cfa2e1
unknown <helloahad21@gmail.com>
2023-07-24 22:02:42 +0600

    new

--- script.js
+++ script.js
@@ -31,3 +31,41 @@ function backspace(){
   var screen = document.getElementById("resultShow");
   screen.value = screen.value.slice(0, -1);
 }
+
+//Light Effect In Light Icon
+
+
+function lightMode(){
+
+  //light icon selected
+  let light = document.getElementById("lightMode");
+
+  //background selected
+  let background = document.getElementById("calculator");
+  //keys div
+  let keyDiv = document.getElementById("keyDiv");
+
+  //light & Dark Div Selected
+  let lightDarkDiv = document.getElementById("lightDirk");
+
+  //What Is Result
+  let WhatResult = document.getElementById("whatIsResult");
+
+  //ResultInput
+
+  let ResultInput = document.getElementById("resultShow");
+
+  let lightGreenKey = document.querySelectorAll("#lightGreenKey")[2];
+
+  light.style.color = "white";
+  background.style.backgroundColor = "white";
+  keyDiv.style.backgroundColor = "#eeeeee";
+  lightDarkDiv.style.backgroundColor = "#eeeeee";
+  WhatResult.style.backgroundColor = "white";
+  WhatResult.style.color = "black";
+  ResultInput.style.backgroundColor = "white";
+  ResultInput.style.color = "black";
+  lightGreenKey.style.backgroundColor = "white";
+
+  console.log(lightGreenKey);
+}
